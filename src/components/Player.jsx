a6a511593124052b27312f3d4bd4465a848744d4
Jeff Ripke <jripke74@gmail.com>
2023-12-22 13:00:19 -0800

    add isEditing logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
-  function handleEditclick() {
-    setIsEditing(true);
+  function handleEditClick() {
+    setIsEditing(!isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>
 
   if (isEditing) {
-    playerName = <input type="text" required />
+    playerName = <input type="text" required value={name} />
   }
 
   return (
@@ -19,7 +19,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditclick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   )
 }
