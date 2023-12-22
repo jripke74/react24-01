778a4f806bddd09db6980a233951ce0f9b1a2ec2
Jeff Ripke <jripke74@gmail.com>
2023-12-22 10:00:42 -0800

    add Player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "./components/Player";
 
 function App() {
 
@@ -5,15 +6,8 @@ function App() {
   return (
     <div id="game-container">
       <ol id="players">
-        <li>
-          <span className="player-name">Player 1</span>
-          <span className="player-symbol">X</span>
-        </li>
-
-        <li>
-          <span className="player-name">Player 2</span>
-          <span className="player-symbol">O</span>
-        </li>
+        <Player name="Player 1" symbol="X" />
+        <Player name="Player 2" symbol="O" />
       </ol>
     </div>
   );
