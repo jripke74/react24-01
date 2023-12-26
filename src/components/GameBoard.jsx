5cc5129a2fc6eca32c94fe35d69830a2ad481d5c
Jeff Ripke <jripke74@gmail.com>
2023-12-26 11:24:23 -0800

    add setGameTurns

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+export default function GameBoard({ onSelectSquare }) {
   return (
     <ol id="game-board">
-      {GameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={onSelectSquare}>
                   {playerSymbol}
                 </button>
               </li>
