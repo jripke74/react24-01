7169dfe13b4d404f420bc62863f1334911b94856
Jeff Ripke <jripke74@gmail.com>
2023-12-27 07:55:33 -0800

    output log data

--- src/App.jsx
+++ src/App.jsx
@@ -21,32 +21,34 @@ function App() {
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns
       ];
-      
+
       return updatedTurns;
     });
 
   }
 
   return (
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player 
-          initialName="Player 1" 
-          symbol="X" 
-          isActive={activePlayer === "X"}
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
         />
-        <Player 
-          initialName="Player 2"
-          symbol="O"
-          isActive={activePlayer === "O"}
-        />
-      </ol>
-      <GameBoard
-        onSelectSquare={handleSelectSquare}
-        turns={gameTurns}
-      />
-      <Log />
-    </div>
+      </div>
+      <Log turns={gameTurns} />
+    </main>
   );
 }
 
